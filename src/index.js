ed6477fe685eec9dbaa24179c1ce8aec244bc2e5
Charles Pick <charles.pick@gmail.com>
2015-10-31 02:50:51 +0000

    support return types, object shapes, arguments etc

--- src/index.js
+++ src/index.js
@@ -23,12 +23,89 @@ export default function ({types: t, template}): Object {
   const checks = createChecks();
 
   const checkIsArray = template(`Array.isArray(input)`);
+  const checkIsObject = template(`input != null && typeof input === 'object'`);
+  const checkNotNull = template(`input != null`);
 
+  const declareTypeChecker = template(`
+    let id = function id (input) {
+      return check;
+    };
+  `);
+
+  const guard = template(`
+    if (!check) {
+      throw new TypeError(message);
+    }
+  `);
+
+  const thrower = template(`
+    if (check) {
+      ret;
+    }
+    else {
+      throw new TypeError(message);
+    }
+  `);
+
+  const stack = [];
 
   return {
     visitor: {
-      TypeAlias (path: Object, file: Object) {
+      TypeAlias (path: Object) {
         path.replaceWith(createTypeAliasChecks(path));
+      },
+
+      Function: {
+        enter (path: Object) {
+          const {node, scope} = path;
+          const paramChecks = collectParamChecks(path);
+          if (node.type === "ArrowFunctionExpression" && node.expression) {
+            node.expression = false;
+            node.body = t.blockStatement(t.returnStatement(node.body));
+          }
+          node.body.body.unshift(...paramChecks);
+          stack.push({node, returns: 0});
+        },
+        exit () {
+          const {node, returns} = stack.pop();
+          console.log({returns});
+        }
+      },
+
+      ReturnStatement (path: Object) {
+        const {node, parent, scope} = path;
+        const {node: {returnType}} = stack[stack.length - 1];
+        if (!returnType || node.isTypeChecked) {
+          return;
+        }
+        stack[stack.length - 1].returns++;
+        let id;
+        if (node.argument.type === 'Identifier') {
+          id = node.argument;
+        }
+        else {
+          id = scope.generateUidIdentifierBasedOnNode(node.argument);
+          scope.push({id: id});
+          path.insertBefore(t.assignmentExpression(
+            '=',
+            id,
+            node.argument
+          ));
+        }
+        const check = checkAnnotation(id, returnType, scope);
+        const ret = t.returnStatement(id);
+        ret.isTypeChecked = true;
+        if (check) {
+          path.replaceWith(thrower({
+            check,
+            ret,
+            message: t.stringLiteral("Return value violates contract.")
+          }));
+        }
+        else {
+          path.replaceWith(ret);
+        }
+        //const check = checkAnnotation(returnType, )
       }
     }
   };
@@ -40,21 +117,23 @@ export default function ({types: t, template}): Object {
       function: template(`typeof input === 'function'`),
       string: template(`typeof input === 'string'`),
       symbol: template(`typeof input === 'symbol'`),
-      object: template(`input != null && typeof input === 'object`),
       undefined: template(`input === undefined`),
       null: template(`input === null`),
       nullOrUndefined: template(`input == null`),
       instanceof: template(`input instanceof type`),
       type: template(`type(input)`),
+      mixed: () => null,
+      any: template(`input != null`),
       union: checkUnion,
-      array: checkArray
+      array: checkArray,
+      object: checkObject
     };
   }
 
-  function checkUnion ({input, types, scope}): Object {
-    const checks = types.map(type => checkAnnotation(input, type, scope));
+  function checkUnion ({input, types, scope}): ?Object {
+    const checks = types.map(type => checkAnnotation(input, type, scope)).filter(identity);
     return checks.reduce((last, check, index) => {
-      if (last === null) {
+      if (last == null) {
         return check;
       }
       return t.logicalExpression(
@@ -72,13 +151,17 @@ export default function ({types: t, template}): Object {
     else if (types.length === 1) {
       const item = t.identifier('item');
       const type = types[0];
+      const check = checkAnnotation(item, type, scope);
+      if (!check) {
+        return checkIsArray(input).expression;
+      }
       return t.logicalExpression(
         '&&',
         checkIsArray(input).expression,
         t.callExpression(
           t.memberExpression(input, t.identifier('every')),
           [t.functionExpression(null, [item], t.blockStatement([
-            t.returnStatement(checkAnnotation(item, type, scope))
+            t.returnStatement(check)
           ]))]
         )
       );
@@ -95,7 +178,7 @@ export default function ({types: t, template}): Object {
           type,
           scope
         )
-      );
+      ).filter(identity);
 
       const checkLength = t.binaryExpression(
         '>=',
@@ -103,13 +186,10 @@ export default function ({types: t, template}): Object {
           input,
           t.identifier('length')
         ),
-        t.numberLiteral(checks.length)
+        t.numberLiteral(types.length)
       );
 
       return checks.reduce((last, check, index) => {
-        if (last === null) {
-          return check;
-        }
         return t.logicalExpression(
           "&&",
           last,
@@ -123,32 +203,48 @@ export default function ({types: t, template}): Object {
     }
   }
 
+  function checkObject ({input, properties, scope}): Object {
+    return properties.reduce((expr, prop, index) => {
+      const check = checkAnnotation(t.memberExpression(input, prop.key), prop.value, scope);
+      if (check) {
+        return t.logicalExpression(
+          "&&",
+          expr,
+          check
+        );
+      }
+      else {
+        return expr;
+      }
+    }, checkIsObject({input}).expression);
+  }
+
   function createTypeAliasChecks (path: Object) {
     const {node, scope} = path;
-
-    const checker = template(`
-      let id = function id (input) {
-        return check;
-      };
-    `);
-
     const {id, right: annotation} = node;
     const input = t.identifier('input');
-    const check = checkAnnotation(input, annotation, scope);
-
-    const declaration = checker({id, check});
+    const check = checkAnnotation(input, annotation, scope) || t.booleanLiteral(true);
+    const declaration = declareTypeChecker({id, check});
     declaration.isTypeChecker = true;
     return declaration;
   }
 
   function checkAnnotation (input: Object, annotation: Object, scope: Object): Object {
     switch (annotation.type) {
+      case 'TypeAnnotation':
+        return checkAnnotation(input, annotation.typeAnnotation, scope);
       case 'GenericTypeAnnotation':
         if (annotation.id.name === 'Array') {
           return checks.array({input, types: annotation.typeParameters ? annotation.typeParameters.params : [], scope});
         }
+        else if (annotation.id.name === 'Function') {
+          return checks.function({input}).expression;
+        }
         else if (isTypeChecker(annotation.id, scope)) {
           return checks.type({input, type: annotation.id}).expression;
+        }
+        else if (isGenericType(annotation.id, scope)) {
+          return;
         }
         else {
           return checks.instanceof({input, type: annotation.id}).expression;
@@ -164,6 +260,12 @@ export default function ({types: t, template}): Object {
         return checks.string({input}).expression;
       case 'UnionTypeAnnotation':
         return checks.union({input, types: annotation.types, scope});
+      case 'ObjectTypeAnnotation':
+        return checks.object({input, properties: annotation.properties, scope});
+      case 'MixedTypeAnnotation':
+        return checks.mixed({input});
+      case 'AnyTypeAnnotation':
+        return checks.any({input}).expression;
       default:
         throw new Error('Unknown type: ' + annotation.type);
     }
@@ -178,7 +280,48 @@ export default function ({types: t, template}): Object {
     return path != null && path.type === 'VariableDeclaration' && path.node.isTypeChecker;
   }
 
+  function isGenericType (id: Object, scope: Object): Boolean {
+    const binding = scope.getBinding(id.name);
+    if (binding !== undefined) {
+      return false;
+    }
+    let {path} = scope;
+    while (path && path.type !== 'Program') {
+      const {node} = path;
+      if (t.isFunction(node) && node.typeParameters) {
+        for (let param of node.typeParameters.params) {
+          if (param.name === id.name) {
+            return true;
+          }
+        }
+      }
+      path = path.parent;
+    }
+    return false;
+  }
 
+  function collectParamChecks ({node, scope}): Array {
+    return node.params.map(param => {
+      if (!param.typeAnnotation) {
+        return;
+      }
+      return createParamGuard(param, scope);
+    }).filter(identity);
+  }
+
+  function createParamGuard (node: Object, scope: Object): ?Object {
+    const check = checkAnnotation(node, node.typeAnnotation, scope);
+    if (!check) {
+      return;
+    }
+    const message = t.stringLiteral(
+      `Argument "${node.name}" violates contract.`
+    );
+    return guard({
+      check,
+      message
+    });
+  }
 
   /**
    * Determine whether the given node can produce purely boolean results.
@@ -211,7 +354,7 @@ export default function ({types: t, template}): Object {
 
 
   /**
-   * Determing whether a given node is nully (null or undefined).
+   * Determine whether a given node is nully (null or undefined).
    */
   function isNodeNully (node: ?Object): boolean {
     if (node == null) {
--- test-polyfill.js
+++ test-polyfill.js
@@ -1,3 +1,4 @@
-require("babel/register")({
-  "stage": 0
+require("babel-core/register")({
+  "presets": ["es2015", "react"],
+  "plugins": ["syntax-flow", "transform-es2015-modules-commonjs"]
 });
