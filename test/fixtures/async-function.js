b752d47972c3faeb02da6a4b6d98796de00f4b87
Charles Pick <charles.pick@gmail.com>
2015-11-21 21:43:40 +0000

    add tests for async functions

---
+++ test/fixtures/async-function.js
@@ -0,0 +1,3 @@
+export default async function demo (input: string[]): string {
+  return input[0];
+}
---
+++ test/fixtures/bad-async-function.js
@@ -0,0 +1,3 @@
+export default async function demo (input: number[]): string {
+  return input;
+}
