654ad2b37538373d8e241b4749e58d25ea2dd179
John Days <johnydays0@gmail.com>
2016-04-06 13:20:51 +0000

    Added tests for e059c1f

---
+++ test/fixtures/bug-xxx-literal-return.js
@@ -0,0 +1,13 @@
+export function demoBoolean (): { property: true } {
+  return { property: true };
+}
+
+export function demoString (): { property: "string" } {
+  return { property: "string" };
+}
+
+export function demoNumber (): { property: 2 } {
+  return { property: 2 };
+}
+
+export default demoBoolean
